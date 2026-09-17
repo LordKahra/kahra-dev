52adcb8bcd84443d1abf59fb2898843444ac9b77
RaiderKing <chaoslfg@gmail.com>
2026-09-17 02:12:39 -0400

    Redesign

--- src/view/page/Page.php
+++ src/view/page/Page.php
@@ -70,8 +70,8 @@ function renderHeaderStart() {
     </div>
 
     <ul>
-        <li><a href="<?=SITE_HOST;?>/#experience-heading">Experience</a></li>
         <li><a href="<?=SITE_HOST;?>/#skillset-heading">Skillset</a></li>
+        <li><a href="<?=SITE_HOST;?>/#experience-heading">Experience</a></li>
         <li><a href="<?=SITE_HOST;?>/#portfolio">Works</a></li>
         <li><a href="<?=SITE_HOST;?>/#about">About</a></li>
     </ul>
